30d4e888476c91b641b0abf786ea17244cdb79e7
WarisAli-source <warissid41@gmail.com>
2021-07-04 15:44:32 +0530

    first commit

---
+++ src/components/UI/Waris.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Waris = () => {
+    return (
+        <div className="copydiv">
+            <p>Copyright @Waris Ali 2021</p>
+        </div>
+    )
+}
+export default Waris;
